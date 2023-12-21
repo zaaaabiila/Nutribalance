ea6049ef160fdfceaef37141d5bc8303409ccca8
Kristin Zega <kristianni.zega08@gmail.com>
2023-12-21 16:14:05 +0700

    fix bug secret manager

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const admin = require('firebase-admin');
 const Hapi = require('@hapi/hapi');
-const serviceAccount = require('./serviceAccountKey.json');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+
+const projectId = 'nutribalance-project';
+
+const getSecret = async (secretName) => {
+  const client = new SecretManagerServiceClient();
+  const [version] = await client.accessSecretVersion({
+    name: `projects/${projectId}/secrets/${secretName}/versions/latest`,
+  });
+  return version.payload.data.toString();
+};
+
+const serviceAccountString = await getSecret('service-account-key');
+const serviceAccount = JSON.parse(serviceAccountString);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

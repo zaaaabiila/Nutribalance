cdaa5bc00907a6920e2c7cd1b461ab9c32efa2ab
Kristin Zega <kristianni.zega08@gmail.com>
2023-12-21 15:29:28 +0700

    fix bug host value

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const nutritionRoutes = require('./routes/nutritionRoute');
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
